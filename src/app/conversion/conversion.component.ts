77f94c450c83ae5d90686a88a38eb66ceeb9be37
Vladimir Bondar <vladi.bondar@gmail.com>
2022-05-04 16:16:47 +0200

    Input currencies to ConversionComponent

--- src/app/conversion/conversion.component.ts
+++ src/app/conversion/conversion.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-conversion',
-  templateUrl: './conversion.component.html',
-  styleUrls: ['./conversion.component.css']
+    selector: 'app-conversion',
+    templateUrl: './conversion.component.html',
+    styleUrls: ['./conversion.component.css'],
 })
-export class ConversionComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ConversionComponent {
+    @Input() currencies: any = [];
 }
